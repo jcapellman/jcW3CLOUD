003294dd204f77557c20b5381b5dcefc13c17b90
Jarred Capellman <jcapellman@hotmail.com>
2015-06-13 21:18:54 -0400

    Initial work on the Bookmark functionality

---
+++ jcW3CLOUD.Lib/BookmarkItem.h
@@ -0,0 +1,8 @@
+#ifndef __BOOKMARKITEM__
+#define __BOOKMARKITEM__
+
+class BookmarkItem {
+	public:
+		BookmarkItem(string description, url);
+};
+#endif
---
+++ jcW3CLOUD.Lib/BookmarkManager.h
@@ -0,0 +1,20 @@
+#ifndef __BOOKMARKMANAGER__
+#define __BOOKMARKMANAGER__
+
+#include "Main.h"
+#include "BookmarkItem.h"
+
+class BookmarkManager {
+	public:
+		BookmarkManager(string fileName);
+		BookmarkManager();
+
+		bool AddBookmark(string description, string url);
+		bool RemoveBook(int id);
+
+		vector<BookmarkItem> GetAll();
+	private:
+		vector<BookmarkItem> _bookmarks;
+};
+
+#endif
